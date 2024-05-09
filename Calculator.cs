cdef35cbcfce7a36bb645c230d1684c59cbd214b
DaveCorleto <davide.corleto@fastwebnet.it>
2024-05-09 15:06:08 +0200

    Aggiungere i file di progetto.

---
+++ Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public class Calculator
+    {
+        public float Somma(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Sottrazione(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public float Divisione(float num1, float num2)
+        {
+            if (num2 == 0)
+            {
+                throw new ArgumentException("Impossibile dividere per zero.");
+            }
+            return num1 / num2;
+        }
+
+        public float Moltiplicazione(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
